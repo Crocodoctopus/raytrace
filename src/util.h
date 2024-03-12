f1929d90b7f8d63a4d3119d66d8a62ea455f715c
akourne <crocodoctopus@gmail.com>
2024-03-11 18:11:22 -0800

    add utils

---
+++ src/util.h
@@ -0,0 +1,12 @@
+#include <vulkan/vulkan.h>
+#include <stdint.h>
+#include <string.h>
+
+#define IS_ZERO(t) memcheck(&t, 0 sizeof(t))
+#define IS_ZERO_PTR(t) memcheck(t, 0, sizeof(*t))
+#define ZERO(t) memset(&t, 0, sizeof(t))
+
+int memcheck(void *ptr, uint8_t val, size_t size);
+size_t strlcpy(char *dst, const char *src, size_t size);
+size_t strlcat(char *dst, const char *src, size_t size);
+const char *vk_result_to_string(VkResult result);
